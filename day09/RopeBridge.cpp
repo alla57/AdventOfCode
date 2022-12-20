c0c943e2450bad8f2e3fabd8063737c54d2f7310
Alla eddine BOUMEDIENE <alboumed@e3r2p18.clusters.42paris.fr>
2022-12-20 16:26:51 +0100

    rope bridge part1 done

--- day09/RopeBridge.cpp
+++ day09/RopeBridge.cpp
@@ -3,7 +3,6 @@
 #include <map>
 #include <set>
 #include <utility>
-#include <cctype>
 
 void	getCloser(std::pair<int, int>& hPos, std::pair<int, int>& tPos, std::set< std::pair<int, int> >& res)
 {
@@ -14,12 +13,10 @@ void	getCloser(std::pair<int, int>& hPos, std::pair<int, int>& tPos, std::set< s
 			++tPos.first;
 		else if (hPos.first - tPos.first <= -1)
 			--tPos.first;
-		res.insert(tPos);
 		if (hPos.second - tPos.second >= 1)
 			++tPos.second;
-		else if (hPos.second - tPos.second < -1)
+		else if (hPos.second - tPos.second <= -1)
 			--tPos.second;
-		res.insert(tPos);
 	}
 	else
 	{
@@ -31,8 +28,8 @@ void	getCloser(std::pair<int, int>& hPos, std::pair<int, int>& tPos, std::set< s
 			++tPos.second;
 		else if (hPos.second - tPos.second < -1)
 			--tPos.second;
-		res.insert(tPos);
 	}
+	res.insert(tPos);
 }
 
 bool isTooFar(std::pair<int, int>& hPos, std::pair<int, int>& tPos)
@@ -47,7 +44,6 @@ bool isTooFar(std::pair<int, int>& hPos, std::pair<int, int>& tPos)
 int main()
 {
 	std::ifstream inputFile("./RopeBridgeInput");
-	// freopen("./RopeBridgeInput", "w", stdin);
 	int x = 0, y = 0;
 	std::pair<int, int> hPos(0, 0);
 	std::pair<int, int> tPos(0, 0);
@@ -56,12 +52,9 @@ int main()
 	std::map< std::string, std::pair<int, int> > moves = {{"L", {-1, 0}}, {"R", {1, 0}}, {"U", {0, 1}}, {"D", {0, -1}}};
 	std::string direction;
 	int step;
-	int k = 0;
 	while (!inputFile.eof())
 	{
-		++k;
 		inputFile >> direction >> step;
-		std::cout << "STEP " << k << std::endl;
 		for (int i = 0; i < step; ++i)
 		{
 			hPos.first += moves[direction].first;
@@ -70,10 +63,6 @@ int main()
 			{
 				getCloser(hPos, tPos, res);
 			}
-			std::cout << "hPos {" << hPos.first << ", " << hPos.second << "}" << std::endl;
-			std::cout << "tPos {" << tPos.first << ", " << tPos.second << "}" << std::endl;
-			std::cout << "moves: " << res.size() << std::endl;
-			std::cout << std::endl;
 		}
 	}
 	std::cout << res.size() << std::endl;
